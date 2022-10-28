08312481c92c7285770673de89fb7cdd83ed11eb
J. Ellis <jgellis63@gmail.com>
2022-10-27 23:37:57 -0600

    setup the pages and a basic navbar

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
+import React from 'react';
+import { ReactDOM } from 'react-dom/client';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import logo from './logo.svg';
+import NavBar from './pages/NavBar';
+import NFL from './pages/NFL';
+import NBA from './pages/NBA';
+import Home from './pages/Home';
+import NoPage from './pages/NoPage';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default function App() {
+    return (
+        <BrowserRouter>
+            <Routes>
+                <Route path="/" element={<NavBar />}>
+                    <Route index element={<Home />} />
+                    <Route path='/nba' element={<NBA />} />
+                    <Route path="/nfl" element={<NFL />} />
+                    <Route path="*" element={<NoPage />} />
+                </Route>
+            </Routes>
+        </BrowserRouter>
+    );
 }
-
-export default App;
