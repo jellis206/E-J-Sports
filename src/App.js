a319316b39d3ace2ed28c66b391b25ec1db76709
J. Ellis <jgellis63@gmail.com>
2022-11-01 12:20:49 -0600

    build should work work for real

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 function App() {
     return (
         <>
-            <Router>
+            <Router basename='/React-E-J-sports/build'>
                 <Navbar />
                 <Routes>
                     <Route path='/' exact element={ <Home /> } />
